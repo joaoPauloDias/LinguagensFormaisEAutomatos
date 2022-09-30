df43fb2ad8466124cc3e3afa9c60314792ca3b1f
Eduardo Mattje <eduardomattje@gmail.com>
2022-09-30 11:13:09 -0300

    Adicionando os tipos

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+from typing import List, Tuple, Dict
+
+State = str
+
+Symbol = str
+
+DistinctionTable = Dict[State, Dict[State, List[State]]]
+
+Transitions = List[Tuple[State, Symbol, State]]
+
+StringList = List[str]
+
+StateGraph = Dict[State, List[State]]
+
 import pydot
+
 
 def dfs(visited, graph, node):
     if node not in visited:
@@ -8,14 +23,15 @@ def dfs(visited, graph, node):
 
 
 class Afd:
-    name = ""
-    states = []
-    alphabet = []
-    initial = None
-    finals = []
-    transitions = []
+    name: str = ""
+    states: List[State] = []
+    alphabet: List[Symbol] = []
+    initial: State = str(None)
+    finals: List[State] = []
+    transitions: Transitions = []
 
-    def __init__(self, name, states, alphabet, initial, finals, transitions):
+    def __init__(self, name: str, states: List[State], alphabet: List[Symbol],
+                 initial: State, finals: List[State], transitions: Transitions):
         self.name = name
         self.states = states
         self.alphabet = alphabet
@@ -23,7 +39,7 @@ class Afd:
         self.finals = finals
         self.transitions = transitions
 
-    def remove_state(self, state):
+    def remove_state(self, state: State):
         if state in self.states:
             self.states.remove(state)
         if state in self.finals:
@@ -33,7 +49,8 @@ class Afd:
     # basicamente remove os estados que não são finais e não levam a algum final
     def remove_useless_states(self):
         graph = {}
-        # inicializa lista de adjacencia
+
+        # inicializa lista de adjacência
         for state in self.states:
             graph[state] = []
         for transition in self.transitions:
@@ -41,6 +58,7 @@ class Afd:
                 graph[transition[0]].append(transition[2])
 
         useless_states = []
+
         # para cada estado
         for state in self.states:
             # se estado for final ignora
@@ -56,20 +74,22 @@ class Afd:
         for useless_state in useless_states:
             self.remove_state(useless_state)
 
-    def remove_unreachable_states(self):
-        graph = {}
+    def remove_unreachable_states(self) -> None:
+        graph: StateGraph = {}
+
         # inicializa lista de adjacencia
         for state in self.states:
             graph[state] = []
+
         for transition in self.transitions:
             if transition[2] not in graph[transition[0]]:
                 graph[transition[0]].append(transition[2])
 
         visited = set()
 
-        # inicia dfs partindo do inicio
+        # inicia dfs partindo do início
         dfs(visited, graph, self.initial)
-        # todos estados nao visitados sao considerados inalcancaveis e excluidos
+        # todos estados não visitados sao considerados inalcancaveis e excluidos
         unreachable_states = list(filter(lambda s: s not in visited, self.states))
         for unreachable_state in unreachable_states:
             self.remove_state(unreachable_state)
@@ -78,9 +98,9 @@ class Afd:
     def check_total(self):
         # para cada estado do afd
         for state in self.states:
-            # pega os caracteres das transicoes que tem como o origem o estado atual
+            # pega os caracteres das transicoes que tem como origem o estado atual
             filtered = [transition[1] for transition in self.transitions if transition[0] == state]
-            # e ve se esses caracteres correspondem a totalidade dos simbolos do alfabeto aceito
+            # e ve se esses caracteres correspondem à totalidade dos símbolos do alfabeto aceito
             not_included = list(filter(lambda char: char not in filtered, self.alphabet))
             # caso nao o automato e considerado nao total
             if len(not_included) > 0:
@@ -90,24 +110,26 @@ class Afd:
 
     # torna automato total
     def make_total(self):
+        DUMP_STATE: State = 'dump'
         # se automato e total retorna
         if self.check_total():
             return
+        # se não for total, prosseguimos com sua transformação
         # adiciona estado de dump nos estados do automato
-        if 'dump' not in self.states:
-            self.states.append('dump')
+        elif DUMP_STATE not in self.states:
+            self.states.append(DUMP_STATE)
         # para cada estado do automato
         for state in self.states:
-            # pega os caracteres das transicoes que tem como o origem o estado atual
+            # pega os caracteres das transicoes que tem como origem o estado atual
             filtered = [transition[1] for transition in self.transitions if transition[0] == state]
-            # e pega todos simbolos do alfabeto que nao estao nessaa lista
+            # e pega todos simbolos do alfabeto que nao estao nessa lista
             not_included = list(filter(lambda symbol: symbol not in filtered, self.alphabet))
             # para cada simbolo nao abrangido por esse estado cria uma transicao que tem como origem ele e o destino
             # o estado de dump
             for char in not_included:
-                self.transitions.append((state, char, 'dump'))
+                self.transitions.append((state, char, DUMP_STATE))
 
-    def mark_distinct_table(self, table, q1, q2):
+    def mark_distinct_table(self, table: DistinctionTable, q1: State, q2: State):
         # para cada celula da tabela de distincao
         for key in table:
             for key_aux in table[key]:
@@ -126,13 +148,23 @@ class Afd:
             'invalido')
 
     def make_distict_table(self):
-        table = {}
-        column_i = self.states[:-1]
-        column_j = self.states[1:]
-        # montar tabela de distincao
-        for index in range(len(column_j)):
+        """
+        Monta a tabela triangular de distinção dos estados.
+        É arranjada do seguinte modo:
+        I s
+        I s s
+        I s s s
+          J J J
+        """
+        table: DistinctionTable = {}
+        column_i = self.states[:-1] # Pega todos menos o último estado
+        line_j = self.states[1:] # Pega todos menos o primeiro estado
+
+        # inicializa tabela de distincão
+        for index in range(len(line_j)):
             table[column_i[index]] = {}
-            for aux_state in column_j[index:]:
+            for aux_state in line_j[index:]:
+                # Adiciona o estado da coluna i, e cria uma lista de dependências ligada ao estado da coluna j
                 table[column_i[index]][aux_state] = []
 
         # marca finais != iniciais
@@ -148,14 +180,13 @@ class Afd:
                 if not table[key][key_aux]:
                     # para cada simbolo do alfabeto
                     for character in self.alphabet:
-                        # encontra os estados destino tendo como origem os dois estados que identificam a celula (
-                        # linha e coluna)
+                        # encontra os estados destino tendo como origem os dois estados que identificam a celula (linha e coluna)
                         destiny_key = self.find_transition(key, character)
                         destiny_key_aux = self.find_transition(key_aux, character)
                         # caso os estados destino sejam iguais ignora
                         if destiny_key == destiny_key_aux:
                             continue
-                        # caso a celula esteja com linha e coluna trocadas na tabela
+                        # caso a celula esteja com linha e coluna trocadas na tabela,
                         # apenas inverte elas
                         try:
                             table[destiny_key][destiny_key_aux]
@@ -170,39 +201,44 @@ class Afd:
                             self.mark_distinct_table(table, key, key_aux)
                             break
                         else:
-                            # caso contrario adiciona a celula destino como uma dependencia da celula atual
+                            # caso contrario, adiciona a celula destino como uma dependencia da celula atual
                             table[key][key_aux].append((destiny_key, destiny_key_aux))
+
         return table
 
-    def unify_states(self):
+    def unify_states(self) -> None:
         # cria a tabela de distincao preenchida
-        table = self.make_distict_table()
+        table: DistinctionTable = self.make_distict_table()
         # para cada celula da tabela
         for key in table:
             for key_aux in table[key]:
                 # se a celula nao tiver sido marcada como distinta
                 if 'X' not in table[key][key_aux]:
-                    # assume que os estados que identificam a celula sao iguais e cria um novo estado a partir da
-                    # uniao deles
+                    # assume que os estados que identificam a celula são iguais e cria um estado a partir da
+                    # união deles
                     new_state = key + key_aux
                     # troca estados anteriores pela uniao dos mesmos e substitui todas suas ocorrencias no automato
                     self.states = list(filter(lambda state: state != key and state != key_aux, self.states))
                     self.states.append(new_state)
+
                     if key in self.finals or key_aux in self.finals:
                         self.finals = list(filter(lambda state: state != key and state != key_aux, self.finals))
                         self.finals.append(new_state)
+
                     if key == self.initial or key_aux == self.initial:
                         self.initial = new_state
                     new_transitions = []
+
                     for transition in self.transitions:
                         if transition[0] == key or transition[0] == key_aux:
                             transition = (new_state, transition[1], transition[2])
                         if transition[2] == key or transition[2] == key_aux:
                             transition = (transition[0], transition[1], new_state)
                         new_transitions.append(transition)
+
                     self.transitions = list(dict.fromkeys(new_transitions))
 
-    def minimize(self):
+    def minimize(self) -> None:
         # remove estados inalcacaveis do automato
         self.remove_unreachable_states()
         # torna ele total
@@ -212,33 +248,41 @@ class Afd:
         # remove estados inuteis
         self.remove_useless_states()
 
-    def validate_word(self, word):
+    def validate_word(self, word: str) -> bool:
         state = self.initial
+
         for char in word:
             state = self.find_transition(state, char)
             if state == 'invalido':
                 return False
+
         return False if state not in self.finals else True
 
-    def validate_words(self, words):
-        response = {}
+    def validate_words(self, words: List[str]):
+        response: Dict[str, bool] = {}
+
         for word in words:
             response[word] = self.validate_word(word)
+
         return response
 
-    def generate_graphviz(self, name):
+    def generate_graphviz(self, name: str) -> None:
         # só colar o graph_txt no https://dreampuf.github.io/GraphvizOnline
-        graph_txt = 'digraph {\nranksep=0.5 size=\"8, 8\"\nrankdir=LR\nInitial [label="" fontsize=14.0 shape=point]\n'
+        graph_txt = f'digraph {{\nranksep=0.5 size=\"8, 8\"\nrankdir=LR\nlabelloc="t"\nlabel={name}\nInitial [label="" fontsize=14.0 shape=point]\n'
+
         for state in self.states:
             if state in self.finals:
                 graph_txt += f'{state} [fontsize=14.0 shape=doublecircle]\n'
             else:
                 graph_txt += f'{state} [fontsize=14.0 shape=circle]\n'
+
         graph_txt += f"Initial -> {self.initial} [arrowsize=0.85]\n"
+
         for transition in self.transitions:
             graph_txt += f'	{transition[0]} -> {transition[2]} [label=\" {transition[1]} \" arrowsize=0.85 fontsize=14.0]\n'
+
         graph_txt += '}'
-        graph, = pydot.graph_from_dot_data(graph_txt)
+        graph = pydot.graph_from_dot_data(graph_txt)[0]
         graph.write_png(f'{name}.png')
 
 
@@ -274,6 +318,7 @@ if __name__ == '__main__':
          ('q2', 'T', 'q2'), ('q2', 'F', 'q3'), ('q3', 'A', 'q2'), ('q3', 'Z', 'q3'), ('q3', 'T', 'q3'),
          ('q3', 'L', 'q4'), ('q4', 'A', 'q2'), ('q4', 'Z', 'q4'), ('q4', 'T', 'q4'), ('q4', 'L', 'q4')]
     My_Afd = Afd(M, S, A, i, F, T)
+
     accepted_words_list = ['ACFL', 'ZTACFL', 'LACFLL', 'AZCTFL', 'LZT']
     rejected_words_list = ['AR', 'ACFR', 'F', 'ACA', 'ACL']
     print(My_Afd.validate_words(accepted_words_list))
